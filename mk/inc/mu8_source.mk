9e9060264f4a7d8ff4cc1b5049baafcfd9aa9725
mu578 <19998532+mu578@users.noreply.github.com>
2023-02-12 10:33:49 -0800

    mk generic

---
+++ mk/inc/mu8_source.mk
@@ -0,0 +1,38 @@
+#
+# # -*- coding: utf-8, tab-width: 3 -*-
+
+#                                                           ___                                            #
+#                                                          / _ \                                           #
+#                                            _   _  _   _ | | | |                                          #
+#                                           | | | || | | || | | |                                          #
+#                                           | |_| || |_| || |_| |                                          #
+#                                           | ._,_| \___/  \___/                                           #
+#                                           | |                                                            #
+#                                           |_|                                                            #
+
+# mu8_source.mk
+#
+# Copyright (C) 2023 mu578. All rights reserved.
+#
+
+define walk-dir-recursive
+	$(wildcard $(1)) $(foreach e, $(wildcard $(1)/*), $(call walk-dir-recursive, $(e)))
+endef
+
+define walk-dir
+	$(wildcard $(1)) $(foreach e, $(wildcard $(1)/*), $(e))
+endef
+
+ifeq ($(strip $(MU8_ROOT_PATH)),)
+$(error MU8_ROOT_PATH is not set)
+endif
+
+MU8_MODULE_NAME  ?= mu8
+MU8_MODULE_PATH  ?= $(MU8_ROOT_PATH)
+MU8_SOURCE_FILES ?= $(call walk-dir-recursive, $(MU8_MODULE_PATH)/source)
+MU8_FILTER_FILES ?= $(filter %.c, $(MU8_SOURCE_FILES))
+
+LOCAL_SRC_FILES += $(MU8_FILTER_FILES)
+LOCAL_CFLAGS    += -I$(MU8_MODULE_PATH)
+
+# EOF
---
+++ mk/mu8.mk
@@ -0,0 +1,33 @@
+#
+# # -*- coding: utf-8, tab-width: 3 -*-
+
+#                                                           ___                                            #
+#                                                          / _ \                                           #
+#                                            _   _  _   _ | | | |                                          #
+#                                           | | | || | | || | | |                                          #
+#                                           | |_| || |_| || |_| |                                          #
+#                                           | ._,_| \___/  \___/                                           #
+#                                           | |                                                            #
+#                                           |_|                                                            #
+
+# mu0.mk
+#
+# Copyright (C) 2023 mu578. All rights reserved.
+#
+
+LOCAL_MODULE      := mu8
+LOCAL_MODULE_PATH := $(dir $(abspath ..))$(LOCAL_MODULE)
+MU8_ROOT_PATH     := $(LOCAL_MODULE_PATH)
+MU0_ROOT_PATH     := $(dir $(abspath ..))mu0
+
+include $(MU8_ROOT_PATH)/mk/inc/mu8_source.mk
+include $(MU0_ROOT_PATH)/mk/inc/mu0_source.mk
+include $(MU0_ROOT_PATH)/mk/inc/mu0_toolchain.mk
+include $(MU0_ROOT_PATH)/mk/inc/mu0_ruleset.mk
+
+all:    rule_all
+static: rule_static
+shared: rule_shared
+clean:  rule_clean
+
+# EOF
