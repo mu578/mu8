c899ae935449c3024aab7a6cbfefe72ea6447e5d
mu578 <19998532+mu578@users.noreply.github.com>
2023-05-23 17:21:05 -0700

    fast math target

--- source/complex_annex/mu8_zpolar.c
+++ source/complex_annex/mu8_zpolar.c
@@ -21,6 +21,16 @@
 
 void mu8_zpolar_fp128 (mu0_fp128_t * zr, mu0_fp128_t * zi, const mu0_fp128_t rho, const mu0_fp128_t theta)
 {
+#	if MU0_HAVE_FASTMATH
+	*zr = rho * mu8_cos_fp128(theta);
+	if (mu8_isnan_fp128(*zr)) {
+		*zr = mu0_fp128_zero;
+	}
+	*zi = rho * mu8_sin_fp128(theta);
+	if (mu8_isnan_fp128(*zi)) {
+		*zi = mu0_fp128_zero;
+	}
+#	else
 	if (mu8_isnan_fp128(rho) || mu8_signbit_fp128(rho) < mu0_fp128_zero) {
 		*zr = mu0_fp128_nan;
 		*zi = mu0_fp128_nan;
@@ -50,10 +60,21 @@ void mu8_zpolar_fp128 (mu0_fp128_t * zr, mu0_fp128_t * zi, const mu0_fp128_t rho
 			*zi = mu0_fp128_zero;
 		}
 	}
+#	endif
 }
 
 void mu8_zpolar_fp64  (mu0_fp64_t  * zr, mu0_fp64_t  * zi, const mu0_fp64_t  rho, const mu0_fp64_t  theta)
 {
+#	if MU0_HAVE_FASTMATH
+	*zr = rho * mu8_cos_fp64(theta);
+	if (mu8_isnan_fp64(*zr)) {
+		*zr = mu0_fp64_zero;
+	}
+	*zi = rho * mu8_sin_fp64(theta);
+	if (mu8_isnan_fp64(*zi)) {
+		*zi = mu0_fp64_zero;
+	}
+#	else
 	if (mu8_isnan_fp64(rho) || mu8_signbit_fp64(rho) < mu0_fp64_zero) {
 		*zr = mu0_fp64_nan;
 		*zi = mu0_fp64_nan;
@@ -83,10 +104,21 @@ void mu8_zpolar_fp64  (mu0_fp64_t  * zr, mu0_fp64_t  * zi, const mu0_fp64_t  rho
 			*zi = mu0_fp64_zero;
 		}
 	}
+#	endif
 }
 
 void mu8_zpolar_fp32  (mu0_fp32_t  * zr, mu0_fp32_t  * zi, const mu0_fp32_t  rho, const mu0_fp32_t  theta)
 {
+#	if MU0_HAVE_FASTMATH
+	*zr = rho * mu8_cos_fp32(theta);
+	if (mu8_isnan_fp32(*zr)) {
+		*zr = mu0_fp32_zero;
+	}
+	*zi = rho * mu8_sin_fp32(theta);
+	if (mu8_isnan_fp32(*zi)) {
+		*zi = mu0_fp32_zero;
+	}
+#	else
 	if (mu8_isnan_fp32(rho) || mu8_signbit_fp32(rho) < mu0_fp32_zero) {
 		*zr = mu0_fp32_nan;
 		*zi = mu0_fp32_nan;
@@ -116,10 +148,21 @@ void mu8_zpolar_fp32  (mu0_fp32_t  * zr, mu0_fp32_t  * zi, const mu0_fp32_t  rho
 			*zi = mu0_fp32_zero;
 		}
 	}
+#	endif
 }
 
 void mu8_zpolar_fp16  (mu0_fp16_t  * zr, mu0_fp16_t  * zi, const mu0_fp16_t  rho, const mu0_fp16_t  theta)
 {
+#	if MU0_HAVE_FASTMATH
+	*zr = rho * mu8_cos_fp16(theta);
+	if (mu8_isnan_fp16(*zr)) {
+		*zr = mu0_fp16_zero;
+	}
+	*zi = rho * mu8_sin_fp16(theta);
+	if (mu8_isnan_fp16(*zi)) {
+		*zi = mu0_fp16_zero;
+	}
+#	else
 	if (mu8_isnan_fp16(rho) || mu8_signbit_fp16(rho) < mu0_fp16_zero) {
 		*zr = mu0_fp16_nan;
 		*zi = mu0_fp16_nan;
@@ -149,6 +192,7 @@ void mu8_zpolar_fp16  (mu0_fp16_t  * zr, mu0_fp16_t  * zi, const mu0_fp16_t  rho
 			*zi = mu0_fp16_zero;
 		}
 	}
+#	endif
 }
 
 /* EOF */
