dc92b1b4c606d55306eb905a66fe2d8f52779c7f
mu578 <19998532+mu578@users.noreply.github.com>
2023-06-19 15:50:56 -0700

    nothing to say

---
+++ misc/linker-main.c
@@ -0,0 +1,34 @@
+//
+// # -*- coding: utf-8, tab-width: 3 -*-
+
+//                                                           ___                                            //
+//                                                          /   \                                           //
+//                                            _   _  _   _  \ O /                                           //
+//                                           | | | || | | | / _ \                                           //
+//                                           | |_| || |_| |( (_) )                                          //
+//                                           | ._,_| \___/  \___/                                           //
+//                                           | |                                                            //
+//                                           |_|                                                            //
+
+// linker-main.c
+//
+// Copyright (C) 2023 mu578. All rights reserved.
+//
+
+#include <mu8/mu8_complex.h>
+#include <mu8/mu8_complex_annex.h>
+#include <mu8/mu8_floating.h>
+#include <mu8/mu8_math.h>
+#include <mu8/mu8_math_aliasing.h>
+#include <mu8/mu8_math_annex.h>
+#include <mu8/mu8_math_constant.h>
+#include <mu8/mu8_math_generic.h>
+
+int main(int argc, const char * argv[])
+{
+	mu0_unused(argc);
+	mu0_unused(argv);
+	return 0;
+}
+
+/* EOF */
---
+++ source/mu8_version.c
@@ -0,0 +1,25 @@
+//
+// # -*- coding: utf-8, tab-width: 3 -*-
+
+//                                                           ___                                            //
+//                                                          /   \                                           //
+//                                            _   _  _   _  \ O /                                           //
+//                                           | | | || | | | / _ \                                           //
+//                                           | |_| || |_| |( (_) )                                          //
+//                                           | ._,_| \___/  \___/                                           //
+//                                           | |                                                            //
+//                                           |_|                                                            //
+
+// mu8_version.c
+//
+// Copyright (C) 2023 mu578. All rights reserved.
+//
+
+#include <mu0/mu0_integer.h>
+#include <mu0/mu0_string.h>
+
+const mu0_uint32_t   __MU8_VERSION_NUMBER__ =  100 ;
+const mu0_tchar8_t * __MU8_VERSION_STRING__ = "100";
+const mu0_tchar8_t * __MU8_VERSION_TOCKEN__ = "mu578@mu8$100";
+
+/* EOF */
