f09f2a77428648d226738d081b79ea8bdde4478b
mu578 <19998532+mu578@users.noreply.github.com>
2023-10-13 10:51:40 -0700

    fix

--- mu8/mu8_functional.h
+++ mu8/mu8_functional.h
@@ -22,7 +22,7 @@
 
 MU0_BEGIN_CDECL
 
-#	if MU0_HAVE_CC_ARMCC || MU0_HAVE_CC_APLCC || MU0_HAVE_CC_CLANG || MU0_HAVE_CC_GNUCC || MU0_HAVE_CC_MSVCL || MU0_HAVE_CC_ITLCL || MU0_HAVE_CC_ITLGC
+#	if 0 // MU0_HAVE_CC_ARMCC || MU0_HAVE_CC_APLCC || MU0_HAVE_CC_CLANG || MU0_HAVE_CC_GNUCC || MU0_HAVE_CC_MSVCL || MU0_HAVE_CC_ITLCL || MU0_HAVE_CC_ITLGC
 
 //#!
 //#! macro<_Tp>(_Tp __x) : _Tp
@@ -164,10 +164,150 @@ MU0_BEGIN_CDECL
 
 #	else
 
+#	if   MU0_HAVE_GENERIC
+#	if   MU0_HAVE_FLOAT128 && MU0_HAVE_FLOAT16 && MU0_HAVE_INT128
+#	define mu8_ini(_Tp, __x) __mu0_generic__((__x) + 0 \
+		, mu0_cfp128_t  : mu0_cfp128 (__x, 0)           \
+		, mu0_cfp64_t   : mu0_cfp64  (__x, 0)           \
+		, mu0_cfp32_t   : mu0_cfp32  (__x, 0)           \
+		, mu0_cfp16_t   : mu0_cfp16  (__x, 0)           \
+		, mu0_fp128_t   : mu0_fp128  (__x)              \
+		, mu0_fp64_t    : mu0_fp64   (__x)              \
+		, mu0_fp32_t    : mu0_fp32   (__x)              \
+		, mu0_fp16_t    : mu0_fp16   (__x)              \
+		, mu0_sint128_t : mu0_sint128(__x)              \
+		, mu0_sint64_t  : mu0_sint64 (__x)              \
+		, mu0_sint32_t  : mu0_sint32 (__x)              \
+		, mu0_sint16_t  : mu0_sint16 (__x)              \
+		, mu0_sint8_t   : mu0_sint8  (__x)              \
+		, mu0_uint128_t : mu0_uint128(__x)              \
+		, mu0_uint64_t  : mu0_uint64 (__x)              \
+		, mu0_uint32_t  : mu0_uint32 (__x)              \
+		, mu0_uint16_t  : mu0_uint16 (__x)              \
+		, mu0_uint8_t   : mu0_uint8  (__x)              \
+		, default       : ((_Tp)__x)                    \
+	)
+#	elif MU0_HAVE_FLOAT128 && MU0_HAVE_FLOAT16
+#	define mu8_ini(_Tp, __x) __mu0_generic__((__x) + 0 \
+		, mu0_cfp128_t  : mu0_cfp128 (__x, 0)           \
+		, mu0_cfp64_t   : mu0_cfp64  (__x, 0)           \
+		, mu0_cfp32_t   : mu0_cfp32  (__x, 0)           \
+		, mu0_cfp16_t   : mu0_cfp16  (__x, 0)           \
+		, mu0_fp128_t   : mu0_fp128  (__x)              \
+		, mu0_fp64_t    : mu0_fp64   (__x)              \
+		, mu0_fp32_t    : mu0_fp32   (__x)              \
+		, mu0_fp16_t    : mu0_fp16   (__x)              \
+		, mu0_sint64_t  : mu0_sint64 (__x)              \
+		, mu0_sint32_t  : mu0_sint32 (__x)              \
+		, mu0_sint16_t  : mu0_sint16 (__x)              \
+		, mu0_sint8_t   : mu0_sint8  (__x)              \
+		, mu0_uint64_t  : mu0_uint64 (__x)              \
+		, mu0_uint32_t  : mu0_uint32 (__x)              \
+		, mu0_uint16_t  : mu0_uint16 (__x)              \
+		, mu0_uint8_t   : mu0_uint8  (__x)              \
+		, default       : ((_Tp)__x)                    \
+	)
+#	elif MU0_HAVE_FLOAT128 && MU0_HAVE_INT128
+#	define mu8_ini(_Tp, __x) __mu0_generic__((__x) + 0 \
+		, mu0_cfp128_t  : mu0_cfp128 (__x, 0)           \
+		, mu0_cfp64_t   : mu0_cfp64  (__x, 0)           \
+		, mu0_cfp32_t   : mu0_cfp32  (__x, 0)           \
+		, mu0_fp128_t   : mu0_fp128  (__x)              \
+		, mu0_fp64_t    : mu0_fp64   (__x)              \
+		, mu0_fp32_t    : mu0_fp32   (__x)              \
+		, mu0_sint128_t : mu0_sint128(__x)              \
+		, mu0_sint64_t  : mu0_sint64 (__x)              \
+		, mu0_sint32_t  : mu0_sint32 (__x)              \
+		, mu0_sint16_t  : mu0_sint16 (__x)              \
+		, mu0_sint8_t   : mu0_sint8  (__x)              \
+		, mu0_uint128_t : mu0_uint128(__x)              \
+		, mu0_uint64_t  : mu0_uint64 (__x)              \
+		, mu0_uint32_t  : mu0_uint32 (__x)              \
+		, mu0_uint16_t  : mu0_uint16 (__x)              \
+		, mu0_uint8_t   : mu0_uint8  (__x)              \
+		, default       : ((_Tp)__x)                    \
+	)
+#	elif MU0_HAVE_FLOAT16 && MU0_HAVE_INT128
+#	define mu8_ini(_Tp, __x) __mu0_generic__((__x) + 0 \
+		, mu0_cfp64_t   : mu0_cfp64  (__x, 0)           \
+		, mu0_cfp32_t   : mu0_cfp32  (__x, 0)           \
+		, mu0_cfp16_t   : mu0_cfp16  (__x, 0)           \
+		, mu0_fp64_t    : mu0_fp64   (__x)              \
+		, mu0_fp32_t    : mu0_fp32   (__x)              \
+		, mu0_fp16_t    : mu0_fp16   (__x)              \
+		, mu0_sint128_t : mu0_sint128(__x)              \
+		, mu0_sint64_t  : mu0_sint64 (__x)              \
+		, mu0_sint32_t  : mu0_sint32 (__x)              \
+		, mu0_sint16_t  : mu0_sint16 (__x)              \
+		, mu0_sint8_t   : mu0_sint8  (__x)              \
+		, mu0_uint128_t : mu0_uint128(__x)              \
+		, mu0_uint64_t  : mu0_uint64 (__x)              \
+		, mu0_uint32_t  : mu0_uint32 (__x)              \
+		, mu0_uint16_t  : mu0_uint16 (__x)              \
+		, mu0_uint8_t   : mu0_uint8  (__x)              \
+		, default       : ((_Tp)__x)                    \
+	)
+#	elif MU0_HAVE_FLOAT128
+#	define mu8_ini(_Tp, __x) __mu0_generic__((__x) + 0 \
+		, mu0_cfp128_t  : mu0_cfp128 (__x, 0)           \
+		, mu0_cfp64_t   : mu0_cfp64  (__x, 0)           \
+		, mu0_cfp32_t   : mu0_cfp32  (__x, 0)           \
+		, mu0_fp128_t   : mu0_fp128  (__x)              \
+		, mu0_fp64_t    : mu0_fp64   (__x)              \
+		, mu0_fp32_t    : mu0_fp32   (__x)              \
+		, mu0_sint64_t  : mu0_sint64 (__x)              \
+		, mu0_sint32_t  : mu0_sint32 (__x)              \
+		, mu0_sint16_t  : mu0_sint16 (__x)              \
+		, mu0_sint8_t   : mu0_sint8  (__x)              \
+		, mu0_uint64_t  : mu0_uint64 (__x)              \
+		, mu0_uint32_t  : mu0_uint32 (__x)              \
+		, mu0_uint16_t  : mu0_uint16 (__x)              \
+		, mu0_uint8_t   : mu0_uint8  (__x)              \
+		, default       : ((_Tp)__x)                    \
+	)
+#	elif MU0_HAVE_FLOAT16
+#	define mu8_ini(_Tp, __x) __mu0_generic__((__x) + 0 \
+		, mu0_cfp64_t   : mu0_cfp64  (__x, 0)           \
+		, mu0_cfp32_t   : mu0_cfp32  (__x, 0)           \
+		, mu0_cfp16_t   : mu0_cfp16  (__x, 0)           \
+		, mu0_fp64_t    : mu0_fp64   (__x)              \
+		, mu0_fp32_t    : mu0_fp32   (__x)              \
+		, mu0_fp16_t    : mu0_fp16   (__x)              \
+		, mu0_sint64_t  : mu0_sint64 (__x)              \
+		, mu0_sint32_t  : mu0_sint32 (__x)              \
+		, mu0_sint16_t  : mu0_sint16 (__x)              \
+		, mu0_sint8_t   : mu0_sint8  (__x)              \
+		, mu0_uint64_t  : mu0_uint64 (__x)              \
+		, mu0_uint32_t  : mu0_uint32 (__x)              \
+		, mu0_uint16_t  : mu0_uint16 (__x)              \
+		, mu0_uint8_t   : mu0_uint8  (__x)              \
+		, default       : ((_Tp)__x)                    \
+	)
+#	elif MU0_HAVE_INT128
+#	define mu8_ini(_Tp, __x) __mu0_generic__((__x) + 0 \
+		, mu0_cfp64_t   : mu0_cfp64  (__x, 0)           \
+		, mu0_cfp32_t   : mu0_cfp32  (__x, 0)           \
+		, mu0_fp64_t    : mu0_fp64   (__x)              \
+		, mu0_fp32_t    : mu0_fp32   (__x)              \
+		, mu0_sint128_t : mu0_sint128(__x)              \
+		, mu0_sint64_t  : mu0_sint64 (__x)              \
+		, mu0_sint32_t  : mu0_sint32 (__x)              \
+		, mu0_sint16_t  : mu0_sint16 (__x)              \
+		, mu0_sint8_t   : mu0_sint8  (__x)              \
+		, mu0_uint128_t : mu0_uint128(__x)              \
+		, mu0_uint64_t  : mu0_uint64 (__x)              \
+		, mu0_uint32_t  : mu0_uint32 (__x)              \
+		, mu0_uint16_t  : mu0_uint16 (__x)              \
+		, mu0_uint8_t   : mu0_uint8  (__x)              \
+		, default       : ((_Tp)__x)                    \
+	)
+#	endif
+#	else
 #	define mu8_ini(_Tp, __x)                   \
 		mu0_is_complex_number(__x)              \
 			? __mu0_extension__ (_Tp) { __x, 0 } \
 			: __mu0_extension__ (_Tp) { __x    }
+#	endif
 
 #	if   MU0_HAVE_GENERIC
 #	if   MU0_HAVE_FLOAT128 && MU0_HAVE_FLOAT16 && MU0_HAVE_INT128
